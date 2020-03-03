ddca8c4f687967000642216db18e3d40e842ddf5
Andrea Walsh <andreawalsh@Andreas-Air.fios-router.home>
2020-03-03 16:35:44 -0500

    Calculator part two

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+function total() {
+  var result;
+  var field1 = document.getElementById('number1').value;
+  var field2 = document.getElementById('number2').value;
+  var symbolName = document.getElementById('symbol').value;
 
- function total () {
-  var field1 = document.getElementById("number1").value;
-  var field2 = document.getElementById("number2").value;
-  var result = parseFloat(field1) + parseFloat(field2);
-
-  if (!isNaN(result)) {
-      document.getElementById("answer").innerHTML = "The answer is " + result;
+  if (symbolName === 'add') {
+    result = parseFloat(field1) + parseFloat(field2);
+  } else if (symbolName === 'subtract') {
+    result = parseFloat(field1) - parseFloat(field2);
+  } else if (symbolName === 'multiply') {
+    result = parseFloat(field1) * parseFloat(field2);
+  } else if (symbolName === 'divide') {
+    result = parseFloat(field1) / parseFloat(field2);
   }
+
+  document.getElementById('answer').innerHTML = 'The answer is ' + result;
+}
+
+function symbolChange() {
+  document.getElementById('calculate').innerHTML = document.getElementById('symbol').value.toUpperCase() || 'CALCULATE';
 }
